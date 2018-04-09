a9284726a8b973d199a33feae8922f44226c4198
Anshula Gandhi <anshula@mit.edu>
2018-04-09 18:24:07 -0400

    swiping closes and opens navbar

---
+++ realanalysis/js/mobile.js
@@ -0,0 +1,25 @@
+$(function(){
+  // Bind the swipeHandler callback function to the swipe event on div.box
+  $( "body" ).on( "swipeleft", swipedLeft );
+  $( "body" ).on( "swiperight", swipedRight );
+ 
+  // Callback function references the event target and adds the 'swipe' class to it
+  function swipedLeft( event ){
+    console.log("swipe left")
+
+    // if navbar is showing, hide it
+    if(!$("#navbarcontainer").hasClass("collapsed")) {
+        $(".navbarcollapsebutton").trigger("click");
+    }
+
+  }
+
+  function swipedRight( event ){
+    console.log("swipe right")
+
+    // if navbar is hidden, show it
+    if($("#navbarcontainer").hasClass("collapsed")) {
+        $(".navbarcollapsebutton").trigger("click");
+    }
+  }
+});
