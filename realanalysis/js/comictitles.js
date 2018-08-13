4ee2d7b3987ab9e6f0c8aadce81d21d2343fea2d
Anshula Gandhi <anshula@mit.edu>
2018-08-13 14:39:29 -0400

    updated 17 on web and pdf
    also added 'real' in front of 'analysis'

--- realanalysis/js/comictitles.js
+++ realanalysis/js/comictitles.js
@@ -1,18 +1,20 @@
 comictitles = [
     {"title" : "1. What is this book?", "newchapter": "true"}, 
-    {"title" : "2. What is analysis?", "newchapter": "true"}, 
-    {"title" : "3. Why do analysis?"},
-    {"title" : "4. Why do analysis books rarely use pictures?"},
+    {"title" : "2. What is real analysis?", "newchapter": "true"}, 
+    {"title" : "3. Why do real analysis?"},
+    {"title" : "4. Why do real analysis books rarely use pictures?"},
     {"title" : "5. How do you prove that something doesn't exist?", "newchapter": "true", "newsubchapter": "Irrationality"},
     {"title" : "6. How do you prove non-existence in math?"},
     {"title" : "7. How do you prove irrationality?"},
     {"title" : "8. Is the square root of two irrational?"},
     {"title" : "9. But do irrational numbers actually exist?"},
     {"title" : "10. How does proving irrationality relate to analysis?"},
-    {"title" : "11. Why does analysis feel so unrelated to calculus?"},
+    {"title" : "11. Why does real analysis feel so unrelated to calculus?"},
     {"title" : "12. How do we know where to put numbers on a number line?", "newsubchapter": "Ordered Sets"},
     {"title" : "13. Does every set have a biggest number?"},
     {"title" : "14. Isn't infinity the biggest element in the set {1,2,3...}?"},
     {"title" : "15. How do you prove infinity isn't an integer?"},
-    {"title" : "16. Does every upper bounded set have a biggest element?"}
+    {"title" : "16. Does every upper bounded set have a biggest element?"},
+    {"title" : "17. What's the most important upper bound of a set?"}
+
 ];
