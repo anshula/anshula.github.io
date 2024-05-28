ead9d16a5cf53902456ee8825cd60fb9f6c7113b
Anshula Gandhi <anshula@users.noreply.github.com>
2024-05-28 16:42:24 +0100

    captions only added after mathjax done, and web reader only done after captions done.

--- compactness/js/math-speech.js
+++ compactness/js/math-speech.js
@@ -1,19 +1,46 @@
 
-function buildMathSpeech() {
 
-   setTimeout(() => {
-      var containers = document.getElementsByTagName("mjx-container");
-      var containersList = Array.prototype.slice.call(containers);
-      containersList.forEach((mjsContainer) => {
-         var mathSpeech = mjsContainer.attributes.getNamedItem("aria-label").value
-         console.log(mathSpeech)
-         var cap = document.createElement("mathcaption")
-         cap.innerHTML = mathSpeech
-         mjsContainer.appendChild(cap)
-      }
+// this can only be done after mathjax has finished rendering
+function createMathCaptions() {
+         var containers = document.getElementsByTagName("mjx-container");
+         var containersList = Array.prototype.slice.call(containers);
+         containersList.forEach((mjsContainer) => {
+            var mathSpeech = mjsContainer.attributes.getNamedItem("aria-label").value
+            var cap = document.createElement("mathcaption")
+            cap.innerHTML = mathSpeech
+            mjsContainer.appendChild(cap)
+         }
       );
+       // console.log('done with math cpations')
    
-   }, "1000"); // wait to do this for 1 second...so after mathjax has been rendered
+
+}
+
+// this can only be done after createMathCaptions
+function createWebReaderWidget() {
+      if (typeof NRWebReader != 'undefined') {
+          window['NRWebReader'] = new NRWebReader({
+            widget_id: "prx8ek8e4z",  // DO NOT REMOVE. This is your widget ID for your WebReader
+            bar : {
+              settings_auto_scroll: true,
+            },
+          });
+      }
+      // console.log('done with widget')
+
+}
+
+function buildMathSpeech() {
+   MathJax.typesetPromise().then(() => {
+      // console.log('done typesetting mathjax')
+      createMathCaptions()
+      createWebReaderWidget()
+
+      MathJax.typesetPromise();
+   }).catch((err) => console.log(err.message));
+
+
+}
+
 
      
-}
